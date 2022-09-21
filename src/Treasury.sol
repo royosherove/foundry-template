647a024904e9046e6a9a54a37a50181b5e6bd22d
royo <royo>
2022-09-22 02:00:39 +0300

    max members

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -7,16 +7,24 @@ pragma solidity ^0.8.9;
 contract Treasury {
     event Joined(address indexed who,uint256 indexed contribution);
     error NotPaid();
+    error ClubIsFull();
+    uint max = 10;
+    uint count = 0;
     
     mapping (address => bool) members  ;
 
+    function setMaxMembers(uint _max) public {
+        max=_max;
+    }
     function getBalance() public view  returns (uint256) {
         return address(this).balance;
     }
 
     function join() public payable {
+        if(count+1>max) revert ClubIsFull();
         if(msg.value < 0.1 ether) revert NotPaid();
         members[msg.sender] = true; 
+        count++;
         emit Joined(address(msg.sender),msg.value);
 
     }
--- test/Treasury.tests.sol
+++ test/Treasury.tests.sol
@@ -1,29 +1,46 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
-import "forge-std/Test.sol";
-// import { console } from "forge-std/console.sol";
-// import { PRBTest } from "@prb/test/PRBTest.sol";
+import { Test } from "forge-std/Test.sol";
+import { Cheats } from "forge-std/Cheats.sol";
+import { console } from "forge-std/console.sol";
 import "./../src/Treasury.sol";
-
 
 /// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/forge/writing-tests
-contract TreasuryTest is Test {
-    event Joined(address indexed who,uint256 indexed contribution);
+contract TreasuryTest is Cheats, Test {
+    event Joined(address indexed who, uint256 indexed contribution);
 
-    function testfuzz_join_emitsJoinedEvent(uint amount) public {
+    function test_setMaxMembers_JoineAboveThreshod_reverts() public {
+        Treasury t = new Treasury();
+        t.setMaxMembers(2);
+
+        address alice = vm.addr(100); vm.deal(alice, 1 ether);
+        address bob = vm.addr(200); vm.deal(bob, 1 ether);
+        address mike = vm.addr(300); vm.deal(mike, 1 ether);
+
+        vm.prank(alice);
+        t.join{ value: 0.1 ether }();
+        vm.prank(bob);
+        t.join{ value: 0.1 ether }();
+
+        vm.expectRevert(Treasury.ClubIsFull.selector);
+        vm.prank(mike);
+        t.join{ value: 0.1 ether }();
+    }
+
+    function testfuzz_join_emitsJoinedEvent(uint256 amount) public {
         vm.assume(amount < 100 ether);
         vm.assume(amount > 0.1 ether);
-    
+
         Treasury t = new Treasury();
         console.log(amount);
 
-        vm.expectEmit(true,true,false,true);
-        emit Joined(address(this),amount);
+        vm.expectEmit(true, true, false, true);
+        emit Joined(address(this), amount);
 
-        t.join{value:amount}();
-        
+        t.join{ value: amount }();
+
         assertTrue(t.isMember(address(this)));
     }
 }
