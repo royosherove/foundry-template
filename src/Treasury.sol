55f91caa5636dd51bc8dfa58bd4a1fad201cd121
Roy <roy@osherove.com>
2022-09-21 17:44:54 +0300

    intial test

---
+++ src/Treasury.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Treasury {
+    event Joined(address indexed who,uint256  contribution);
+    error NotPaid();
+    
+    mapping (address => bool) members  ;
+
+    function getBalance() public view  returns (uint256) {
+        return address(this).balance;
+    }
+
+    function join() public payable {
+        if(msg.value < 0.1 ether) revert NotPaid();
+        members[msg.sender] = true; 
+        emit Joined(address(msg.sender),msg.value);
+
+    }
+
+    function isMember(address _address) public view returns (bool){
+       return members[_address];
+    }
+
+    receive() external payable { }
+}
--- test/Treasury.tests.sol
+++ test/Treasury.tests.sol
@@ -2,19 +2,25 @@
 pragma solidity >=0.8.4;
 
 import { Cheats } from "forge-std/Cheats.sol";
+import { Test } from "forge-std/Test.sol";
 import { console } from "forge-std/console.sol";
 import { PRBTest } from "@prb/test/PRBTest.sol";
+import "./../src/Treasury.sol";
+
 
 /// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/forge/writing-tests
-contract ContractTest is PRBTest, Cheats {
-    function setUp() public {
-        // solhint-disable-previous-line no-empty-blocks
-    }
+contract TreasuryTest is PRBTest, Cheats {
+    event Joined(address indexed who,uint256  contribution);
 
-    /// @dev Run Forge with `-vvvv` to see console logs.
     function testExample() public {
-        console.log("Hello World");
-        assertTrue(true);
+        Treasury t = new Treasury();
+
+        vm.expectEmit(true,false,false,false);
+        emit Joined(address(this),0.2 ether);
+
+        t.join{value:0.1 ether}();
+        
+        assertTrue(t.isMember(address(this)));
     }
 }
