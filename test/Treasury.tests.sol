faf246a723d4012cb5161d52f251fea8f535c941
royo <royo>
2022-09-22 01:07:35 +0300

    move to simple test

--- test/Treasury.tests.sol
+++ test/Treasury.tests.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
-import { Cheats } from "forge-std/Cheats.sol";
-import { Test } from "forge-std/Test.sol";
-import { console } from "forge-std/console.sol";
-import { PRBTest } from "@prb/test/PRBTest.sol";
+import "forge-std/Test.sol";
+// import { console } from "forge-std/console.sol";
+// import { PRBTest } from "@prb/test/PRBTest.sol";
 import "./../src/Treasury.sol";
 
 
 /// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
 /// https://book.getfoundry.sh/forge/writing-tests
-contract TreasuryTest is PRBTest, Cheats {
+contract TreasuryTest is Test {
     event Joined(address indexed who,uint256 indexed contribution);
 
     function testfuzz_join_emitsJoinedEvent(uint amount) public {
         vm.assume(amount < 100 ether);
         vm.assume(amount > 0.1 ether);
+    
         Treasury t = new Treasury();
         console.log(amount);
 
